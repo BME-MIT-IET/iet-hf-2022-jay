3a14d9f8297ae788a26551dd71bf6ce0463d53a8
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-16 11:10:00 +0200

    UI test added to check if serviceOffCard appears again when clicking serviceOnCard

---
+++ app/src/androidTest/java/illyan/jay/TurnOffLocationTest.kt
@@ -0,0 +1,173 @@
+package illyan.jay
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class TurnOffLocationTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.ACCESS_FINE_LOCATION",
+            "android.permission.FOREGROUND_SERVICE"
+        )
+
+    @Test
+    fun turnOffLocationTest() {
+        val gz = onView(
+            allOf(
+                withText("Sign in"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.googleSignInButton),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        gz.perform(click())
+
+        val materialCardView = onView(
+            allOf(
+                withId(R.id.serviceOffCard),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.serviceToggleCardFlipper),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialCardView.perform(click())
+
+        val materialCardView2 = onView(
+            allOf(
+                withId(R.id.serviceOffCard),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.serviceToggleCardFlipper),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialCardView2.perform(click())
+
+        val materialCardView3 = onView(
+            allOf(
+                withId(R.id.serviceOnCard),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.serviceToggleCardFlipper),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialCardView3.perform(click())
+
+        val cardView = onView(
+            allOf(
+                withId(R.id.serviceOffCard),
+                withParent(
+                    allOf(
+                        withId(R.id.serviceToggleCardFlipper),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        cardView.check(matches(isDisplayed()))
+
+        val textView = onView(
+            allOf(
+                withId(R.id.serviceOffTitle), withText("Off"),
+                withParent(withParent(withId(R.id.serviceOffCard))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(isDisplayed()))
+
+        val textView2 = onView(
+            allOf(
+                withId(R.id.serviceOffTitle), withText("Off"),
+                withParent(withParent(withId(R.id.serviceOffCard))),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Off")))
+
+        val textView3 = onView(
+            allOf(
+                withId(R.id.serviceOffTitle), withText("Off"),
+                withParent(withParent(withId(R.id.serviceOffCard))),
+                isDisplayed()
+            )
+        )
+        textView3.check(matches(withText("Off")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
