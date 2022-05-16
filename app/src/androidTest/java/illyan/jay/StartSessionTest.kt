9a2ebbfdcdaf4443bf5aa668601d5cc1965f45c6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-16 11:25:49 +0200

    UI test added to check if a new session appears in sessionsRecycler

---
+++ app/src/androidTest/java/illyan/jay/StartSessionTest.kt
@@ -0,0 +1,221 @@
+package illyan.jay
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class StartSessionTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.ACCESS_FINE_LOCATION",
+            "android.permission.FOREGROUND_SERVICE"
+        )
+
+    @Test
+    fun startSessionTest() {
+        val gz = onView(
+            allOf(
+                withText("Sign in"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.googleSignInButton),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        gz.perform(click())
+
+        val materialCardView = onView(
+            allOf(
+                withId(R.id.serviceOffCard),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.serviceToggleCardFlipper),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialCardView.perform(click())
+
+        val materialCardView2 = onView(
+            allOf(
+                withId(R.id.serviceOffCard),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.serviceToggleCardFlipper),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialCardView2.perform(click())
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.realtimeMapNavFragment), withContentDescription("Map"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottomNavigationMain),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val bottomNavigationItemView2 = onView(
+            allOf(
+                withId(R.id.sessionsNavFragment), withContentDescription("Sessions"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottomNavigationMain),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView2.perform(click())
+
+        val viewGroup = onView(
+            allOf(
+                withId(androidx.appcompat.R.id.action_bar),
+                withParent(
+                    allOf(
+                        withId(androidx.appcompat.R.id.action_bar_container),
+                        withParent(withId(androidx.appcompat.R.id.decor_content_parent))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+
+        val textView = onView(
+            allOf(
+                withText("Sessions"),
+                withParent(
+                    allOf(
+                        withId(androidx.appcompat.R.id.action_bar),
+                        withParent(withId(androidx.appcompat.R.id.action_bar_container))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(isDisplayed()))
+
+        val textView2 = onView(
+            allOf(
+                withText("Sessions"),
+                withParent(
+                    allOf(
+                        withId(androidx.appcompat.R.id.action_bar),
+                        withParent(withId(androidx.appcompat.R.id.action_bar_container))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Sessions")))
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.sessionsRecycler),
+                withParent(withParent(withId(R.id.sessionsNavHost))),
+                isDisplayed()
+            )
+        )
+        recyclerView.check(matches(isDisplayed()))
+
+        val cardView = onView(
+            allOf(
+                withId(R.id.sessionCard),
+                withParent(withParent(withId(R.id.sessionsRecycler))),
+                isDisplayed()
+            )
+        )
+        cardView.check(matches(isDisplayed()))
+
+        val frameLayout = onView(
+            allOf(
+                withId(R.id.bottomNavigationMain),
+                withParent(withParent(withId(R.id.loginNavHostFragment))),
+                isDisplayed()
+            )
+        )
+        frameLayout.check(matches(isDisplayed()))
+
+        val frameLayout2 = onView(
+            allOf(
+                withId(R.id.bottomNavigationMain),
+                withParent(withParent(withId(R.id.loginNavHostFragment))),
+                isDisplayed()
+            )
+        )
+        frameLayout2.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
